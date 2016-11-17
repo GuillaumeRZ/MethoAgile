dc5fe988cf7abb82bd1821fbeec0da4c2a6475f3
Vincent Curt <vincent.curt@etu-webschoolfactory.fr>
2016-11-17 10:45:50 +0100

    Fix functions.php, add single-quote and page-list for quotes

---
+++ wp-content/themes/trumpsaid/page-list.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+<?php query_posts('post_per_page=10'); ?>
+<?php if(have_posts()): while(have_posts()): the_post(); ?>
+		<div class="quote">
+			<?php echo do_shortcode("[quotes cate='Trump']") ?>
+			<ul id="vote">
+				<li>&#8593;</li>
+				<li>&#8595;</li>
+				<li>0</li>
+			</ul>
+			<ul id="share">
+				<li>to Trump</li>
+				<li><i class="fa fa-facebook-official" aria-hidden="true"></i></li>
+				<li><i class="fa fa-twitter" aria-hidden="true"></i></li>
+			</ul>
+		</div>
+<?php endwhile; endif;?>
+
+<?php get_footer(); ?>
