dd41ee1001bd0b49676b2683eb583d63a3a3e85e
Come Bouquet <come.bouquet@etu-webschoolfactory.fr>
2016-11-18 12:31:54 +0100

    deleted every useless plugins

--- wp-content/plugins/quote-master/php/qm-shortcodes.php
+++ wp-content/plugins/quote-master/php/qm-shortcodes.php
@@ -123,6 +123,7 @@ class QM_Shortcodes
             $shortcode_each .= '<a href="http://'.$_SERVER["HTTP_HOST"].'/quote/'.$post_slug.'"><span class="qm_quote_text">'.$quote_text.'</span></a>';
 
             $author = get_post_meta(get_the_ID(),'quote_author',true);
+            $twitter = get_post_meta(get_the_ID(), 'twitter', true);
             if ($author != '')
             {
               $author = "~".$author;
@@ -149,10 +150,11 @@ class QM_Shortcodes
           // $shortcode_each .= do_shortcode('[ssba]');
           $shortcode_each .= '
           <ul class="share">
-            <a data-site="" class="ssba_facebook_share" href="http://www.facebook.com/sharer.php?u=http://'.$_SERVER["HTTP_HOST"].'/quote/'.$post_slug.'" target="_blank">
+            <a data-site="" href="http://twitter.com/share?url=http://'.$_SERVER["HTTP_HOST"].'/quote/'.$post_slug.'+'.$twitter.'"<li>to Trump</li></a>
+            <a data-site="" href="http://www.facebook.com/sharer.php?u=http://'.$_SERVER["HTTP_HOST"].'/quote/'.$post_slug.'" target="_blank">
               <li><i class="fa fa-facebook-official" aria-hidden="true"></i></li>
             </a>
-            <a data-site="" class="ssba_twitter_share" href="http://twitter.com/share?url=http://'.$_SERVER["HTTP_HOST"].'/quote/'.$post_slug.'" target="_blank">
+            <a data-site="" href="http://twitter.com/share?url=http://'.$_SERVER["HTTP_HOST"].'/quote/'.$post_slug.'" target="_blank">
               <li><i class="fa fa-twitter" aria-hidden="true" alt="Tweet about this on Twitter"></i></li>
             </a>
           </ul>
