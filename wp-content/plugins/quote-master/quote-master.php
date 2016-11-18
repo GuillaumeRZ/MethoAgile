225675a96a2e63f71180ae8975bc3cf8a94df5aa
Come Bouquet <come.bouquet@etu-webschoolfactory.fr>
2016-11-18 12:40:08 +0100

    added a function to disable update in the quote master plugin

--- wp-content/plugins/quote-master/quote-master.php
+++ wp-content/plugins/quote-master/quote-master.php
@@ -235,4 +235,10 @@ class MLW_Quote_Master
 }
 $quote_master = new MLW_Quote_Master();
 
+add_filter('site_transient_update_plugins', 'remove_update_notification');
+function remove_update_notification($value) {
+     unset($value->response[ plugin_basename(__FILE__) ]);
+     return $value;
+} 
+
 ?>
