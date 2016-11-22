f191fb9160edd7027abdc98fb3ac0cbbb1434d7c
Vincent Curt <vincent.curt@etu-webschoolfactory.fr>
2016-11-22 20:17:23 +0100

    404 page done, site logo added and css for the menu added (need a fix for the logo)

--- wp-content/themes/trumpsaid/404.php
+++ wp-content/themes/trumpsaid/404.php
@@ -1,7 +1,27 @@
-<?php get_header(); ?>
-
+<!doctype html>
+<head>
+	<meta charset="UTF-8">
+	<title><?php bloginfo( 'name' ); ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?> id="error" >
+	<header class="header">
+			<?php
+				wp_nav_menu( array(
+				'theme_location'  => 'main-menu',
+				'menu'            => '',
+				'container'       => 'nav',
+				'container_class' => 'main-menu',
+				'container_id'    => 'main-menu',
+				'echo'            => true,
+				'fallback_cb'     => 'wp_page_menu'
+			) ); ?>
+	</header>
 <div id="errorPage">
-	<h1>Woops, you're on the wrong site of the border</h1>
+	<h1>Woops, it seems you're on the wrong side of the border</h1>
 </div>
 
 <?php get_footer(); ?>
--- wp-content/themes/trumpsaid/header.php
+++ wp-content/themes/trumpsaid/header.php
@@ -8,7 +8,7 @@
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> >
-	<header id="header">
+	<header class="header">
 			<?php
 				wp_nav_menu( array(
 				'theme_location'  => 'main-menu',
